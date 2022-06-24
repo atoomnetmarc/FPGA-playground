30ea3fa43bc0dda7c54b4f88d08d48100332ddaf
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2022-06-24 18:36:14 +0200

    Fsm1

---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Fsm1/top_module_tb.v
@@ -0,0 +1,48 @@
+/*
+
+Copyright 2022 Marc Ketel
+SPDX-License-Identifier: Apache-2.0
+
+*/
+
+`timescale 1s / 10ms
+
+module top_module_tb ();
+  wire out;
+
+  reg clk = 1'd1;
+  reg areset = 1'd0;
+  reg in = 1'd1;
+
+  localparam ITERATIONS = 1000;
+
+  always begin
+    #5 clk = clk + 1;
+  end
+
+  always begin
+    #100 in = 1'b0;
+    #30 in = 1'b1;
+  end
+
+  initial begin
+    #75 areset = 1'b1;
+    #15 areset = 1'b0;
+  end
+
+  top_module uut (
+      .clk(clk),
+      .areset(areset),
+      .in(in),
+      .out(out)
+  );
+
+  initial begin
+    $dumpfile("top_module_tb.vcd");
+    $dumpvars(0, top_module_tb);
+
+    #(ITERATIONS) $display("Done simulating.");
+    $finish;
+  end
+
+endmodule
