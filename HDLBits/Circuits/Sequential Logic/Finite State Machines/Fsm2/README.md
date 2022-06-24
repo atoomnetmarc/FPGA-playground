df27c49617229140e9f18c7e902279a8837aef5d
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2022-06-24 19:20:59 +0200

    Fsm2

---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Fsm2/README.md
@@ -0,0 +1,25 @@
+https://hdlbits.01xz.net/wiki/Fsm2
+
+Diagram:\
+![](diagram.svg)
+
+After syntheses for ice40:\
+![](diagram-ice40.svg)
+
+Netlistsvg:\
+![](netlist.svg)
+
+# Requirements
+
+iCE40HX8K-EVB board.
+
+# Device utilisation
+
+```
+ ICESTORM_LC:     3/ 7680     0%
+ICESTORM_RAM:     0/   32     0%
+       SB_IO:     5/  256     1%
+       SB_GB:     1/    8    12%
+ICESTORM_PLL:     0/    2     0%
+ SB_WARMBOOT:     0/    1     0%
+```
---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Fsm2/top_module_tb.v
@@ -0,0 +1,55 @@
+/*
+
+Copyright 2022 Marc Ketel
+SPDX-License-Identifier: Apache-2.0
+
+*/
+
+`timescale 1s / 10ms
+
+module top_module_tb ();
+  wire out;
+
+  reg clk = 1'd1;
+  reg areset = 1'd0;
+  reg j = 1'd0;
+  reg k = 1'd0;
+
+  localparam ITERATIONS = 1000;
+
+  always begin
+    #5 clk = clk + 1;
+  end
+
+  always begin
+    #50 j = 1'b0;
+    #50 j = 1'b1;
+  end
+
+  always begin
+    #70 k = 1'b0;
+    #70 k = 1'b1;
+  end
+
+  initial begin
+    #212 areset = 1'b1;
+    #1 areset = 1'b0;
+  end
+
+  top_module uut (
+      .clk(clk),
+      .areset(areset),
+      .j(j),
+      .k(k),
+      .out(out)
+  );
+
+  initial begin
+    $dumpfile("top_module_tb.vcd");
+    $dumpvars(0, top_module_tb);
+
+    #(ITERATIONS) $display("Done simulating.");
+    $finish;
+  end
+
+endmodule
