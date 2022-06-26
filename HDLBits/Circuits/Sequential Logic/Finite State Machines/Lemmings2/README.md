63a47d8daac5815e6d5751b5aecf57bdcc446420
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2022-06-26 21:13:26 +0200

    Lemmings2

---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Lemmings2/README.md
@@ -0,0 +1,22 @@
+https://hdlbits.01xz.net/wiki/Lemmings2
+
+Diagram:\
+![](diagram.svg)
+
+Netlistsvg:\
+![](netlist.svg)
+
+# Requirements
+
+iCE40HX8K-EVB board.
+
+# Device utilisation
+
+```
+ ICESTORM_LC:     9/ 7680     0%
+ICESTORM_RAM:     0/   32     0%
+       SB_IO:     8/  256     3%
+       SB_GB:     1/    8    12%
+ICESTORM_PLL:     0/    2     0%
+ SB_WARMBOOT:     0/    1     0%
+```
---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Lemmings2/top_module.v
@@ -0,0 +1,63 @@
+/*
+
+Copyright 2022 Marc Ketel
+SPDX-License-Identifier: Apache-2.0
+
+*/
+
+`default_nettype none
+
+module top_module (
+    input  wire clk,
+    input  wire areset,
+    input  wire bump_left,
+    input  wire bump_right,
+    input  wire ground,
+    output reg  walk_left,
+    output reg  walk_right,
+    output reg  aaah
+);
+
+  parameter LEFT = 2'b00, RIGHT = 2'b01, FALL_L = 2'b10, FALL_R = 2'b11;
+  reg [1:0] state = LEFT, next_state;
+
+  `define GROUND_SOLID 1'b1
+  `define GROUND_ABSENT 1'b0
+  `define VOICE_AAAAAH 1'b1
+  `define VOICE_SILENT 1'b0
+
+  //Determine next state.
+  always @(*) begin
+    casez ({
+      bump_left, bump_right, ground, state
+    })
+      {2'b11, `GROUND_SOLID, LEFT} : next_state = RIGHT;
+      {2'b11, `GROUND_SOLID, RIGHT} : next_state = LEFT;
+      {2'b10, `GROUND_SOLID, LEFT} : next_state = RIGHT;
+      {2'b01, `GROUND_SOLID, RIGHT} : next_state = LEFT;
+      {2'bzz, `GROUND_ABSENT, LEFT} : next_state = FALL_L;
+      {2'bzz, `GROUND_ABSENT, RIGHT} : next_state = FALL_R;
+      {2'bzz, `GROUND_SOLID, FALL_L} : next_state = LEFT;
+      {2'bzz, `GROUND_SOLID, FALL_R} : next_state = RIGHT;
+      default: next_state = state;
+    endcase
+  end
+
+  //State transition.
+  always @(posedge clk, posedge areset) begin
+    if (areset) state <= LEFT;
+    else if (clk) begin
+      state <= next_state;
+    end
+  end
+
+  //Determine output.
+  always @(*) begin
+    case (state)
+      LEFT: {walk_left, walk_right, aaah} = {2'b10, `VOICE_SILENT};
+      RIGHT: {walk_left, walk_right, aaah} = {2'b01, `VOICE_SILENT};
+      FALL_L, FALL_R: {walk_left, walk_right, aaah} = {2'b00, `VOICE_AAAAAH};
+      default: {walk_left, walk_right, aaah} = {2'b00, `VOICE_SILENT};
+    endcase
+  end
+endmodule
