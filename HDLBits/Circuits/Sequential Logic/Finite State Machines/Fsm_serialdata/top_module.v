16c68348232850b847ec634523626b26dcc9c46b
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2022-07-03 13:34:47 +0200

    Fsm_serialdata

---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Fsm_serialdata/top_module.v
@@ -0,0 +1,145 @@
+/*
+
+Copyright 2022 Marc Ketel
+SPDX-License-Identifier: Apache-2.0
+
+*/
+
+`default_nettype none
+
+`define STATE_IDLE 3'b000
+`define STATE_START 3'b001
+`define STATE_DATA 3'b010
+`define STATE_STOP 3'b011
+`define STATE_ILLIGAL 3'b111
+
+module top_module (
+    input wire clk,
+    input wire in,
+    input wire reset,
+    output wire [7:0] out_byte,
+    output wire done
+);
+
+  wire [2:0] state, next_state;
+  wire [4:0] bits;
+  wire [8:0] data;
+
+  //Determine next state.
+  fsm_next_state ns (
+      .in(in),
+      .state(state),
+      .bits(bits),
+      .next_state(next_state)
+  );
+
+  //State transition.
+  fsm_state_transition st (
+      .clk(clk),
+      .in(in),
+      .reset(reset),
+      .next_state(next_state),
+      .bits(bits),
+      .state(state),
+      .data(data)
+  );
+
+  //Determine output.
+  fsm_output o (
+      .state(state),
+      .data(data),
+      .out_byte(out_byte),
+      .done(done)
+  );
+
+endmodule
+
+
+module fsm_next_state (
+    input wire in,
+    input wire [2:0] state,
+    input wire [4:0] bits,
+    output reg [2:0] next_state
+);
+
+  always @(*) begin
+    casez ({
+      in, state, (bits == 4'd8)
+    })
+      //Wait for idle state.
+      {1'b0, `STATE_IDLE, 1'bz} : next_state = `STATE_IDLE;
+
+      //Idle state found.
+      {1'b1, `STATE_IDLE, 1'bz} : next_state = `STATE_START;
+
+      //Wait for start bit.
+      {1'b1, `STATE_START, 1'bz} : next_state = `STATE_START;
+
+      //Start bit found.
+      {1'b0, `STATE_START, 1'bz} : next_state = `STATE_DATA;
+
+      //Wait for reception of all data bits.
+      {1'bz, `STATE_DATA, 1'b0} : next_state = `STATE_DATA;
+
+      //Reception complete but stop bit not found.
+      {1'b0, `STATE_DATA, 1'b1} : next_state = `STATE_IDLE;
+
+      //Reception complete.
+      {1'b1, `STATE_DATA, 1'b1} : next_state = `STATE_STOP;
+
+      //Start bit found.
+      {1'b0, `STATE_STOP, 1'bz} : next_state = `STATE_DATA;
+
+      //Invalid start bit.
+      {1'b1, `STATE_STOP, 1'bz} : next_state = `STATE_START;
+
+      //We forgot a state.
+      {1'bz, `STATE_ILLIGAL, 1'bz} : next_state = `STATE_ILLIGAL;
+      default: next_state = `STATE_ILLIGAL;
+    endcase
+  end
+
+endmodule
+
+module fsm_state_transition (
+    input wire clk,
+    input wire in,
+    input wire reset,
+    input wire [2:0] next_state,
+    output reg [4:0] bits,
+    output reg [2:0] state,
+    output reg [8:0] data
+);
+
+  always @(posedge clk) begin
+    if (reset) state <= `STATE_START;
+    else if (clk) begin
+      state <= next_state;
+
+      case (state)
+        `STATE_START, `STATE_STOP: bits <= 4'd0;
+        `STATE_DATA: bits <= (bits < 4'd8) ? bits + 1'b1 : bits;
+      endcase
+
+      data <= data >> 1;
+      data[8] <= in;
+    end
+  end
+endmodule
+
+module fsm_output (
+    input wire [2:0] state,
+    input wire [8:0] data,
+    output reg [7:0] out_byte,
+    output reg done
+);
+
+  always @(*) begin
+    case (state)
+      `STATE_STOP: done = 1'b1;
+      default: done = 1'b0;
+    endcase
+
+    out_byte = data[7:0];
+  end
+endmodule
---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/Fsm_serialdata/top_module_tb.v
@@ -0,0 +1,77 @@
+/*
+
+Copyright 2022 Marc Ketel
+SPDX-License-Identifier: Apache-2.0
+
+*/
+
+`timescale 1s / 10ms
+
+module top_module_tb ();
+  reg clk = 1'd1;
+  reg reset = 1'd1;
+  reg in = 1'd1;
+  wire [7:0] out_byte;
+  wire done;
+
+  localparam ITERATIONS = 1000;
+
+  always begin
+    #5 clk = clk + 1;
+  end
+
+  always begin
+    #10 in = 1'b0;
+    #90 in = 1'b1;
+    #10 in = 1'b0;
+    #90 in = 1'b1;
+    #10 in = 1'b0;
+    #100;
+
+    #10 in = 1'b1; //idle
+    #30 in = 1'b0; //start
+    #10 in = 1'b1; //bit 1
+    #10 in = 1'b1; //bit 2
+    #10 in = 1'b0; //bit 3
+    #10 in = 1'b1; //bit 4
+    #10 in = 1'b0; //bit 5
+    #10 in = 1'b0; //bit 6
+    #10 in = 1'b1; //bit 7
+    #10 in = 1'b0; //bit 8
+    #10 in = 1'b1; //stop
+
+    #10 in = 1'b1; //idle
+    #30 in = 1'b0; //start
+    #10 in = $random; //bit 1
+    #10 in = $random; //bit 2
+    #10 in = $random; //bit 3
+    #10 in = $random; //bit 4
+    #10 in = $random; //bit 5
+    #10 in = $random; //bit 6
+    #10 in = $random; //bit 7
+    #10 in = $random; //bit 8
+    #10 in = 1'b0; //invalid stop
+
+  end
+
+  initial begin
+    #10 reset = 1'b0;
+  end
+
+  top_module uut (
+      .clk(clk),
+      .reset(reset),
+      .in(in),
+      .out_byte(out_byte),
+      .done(done)
+  );
+
+  initial begin
+    $dumpfile("top_module_tb.vcd");
+    $dumpvars(0, top_module_tb);
+
+    #(ITERATIONS) $display("Done simulating.");
+    $finish;
+  end
+
+endmodule
