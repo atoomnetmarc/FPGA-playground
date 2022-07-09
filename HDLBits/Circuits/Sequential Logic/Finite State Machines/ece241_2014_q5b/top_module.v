32a31f3f6b0c03d09b673234f49f8bab6482d519
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2022-07-09 14:21:04 +0200

    ece241_2014_q5b

---
+++ HDLBits/Circuits/Sequential Logic/Finite State Machines/ece241_2014_q5b/top_module.v
@@ -0,0 +1,35 @@
+/*
+
+Copyright 2022 Marc Ketel
+SPDX-License-Identifier: Apache-2.0
+
+*/
+
+`default_nettype none
+
+`define STATE_A 1'b0
+`define STATE_B 1'b1
+
+module top_module (
+    input  wire clk,
+    input  wire areset,
+    input  wire x,
+    output wire z
+);
+
+  reg  [1:0] state;
+  wire [1:0] next_state;
+
+  //Determine next state.
+  assign next_state[`STATE_A] = state[`STATE_A] & ~x;
+  assign next_state[`STATE_B] = state[`STATE_A] & x | state[`STATE_B];
+
+  //State transition.
+  always @(posedge clk, posedge areset) begin
+    if (areset) state <= (1'b1 << `STATE_A);
+    else if (clk) state <= next_state;
+  end
+
+  //Determine output.
+  assign z = state[`STATE_A] & x | state[`STATE_B] & ~x;
+endmodule
