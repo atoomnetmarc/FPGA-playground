cab318d4754d506d6f973bc50cb2941b5ebba0f0
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2022-07-14 19:35:09 +0200

    defines -> parameters.

--- HDLBits/Circuits/Building Larger Circuits/review2015_fsmseq/top_module.v
+++ HDLBits/Circuits/Building Larger Circuits/review2015_fsmseq/top_module.v
@@ -7,13 +7,6 @@ SPDX-License-Identifier: Apache-2.0
 
 `default_nettype none
 
-`define STATE_X0 3'b000
-`define STATE_X1 3'b001
-`define STATE_X2 3'b010
-`define STATE_X3 3'b011
-`define STATE_F0 3'b100
-`define STATE_ILLEGAL 3'b111
-
 module top_module (
     input  wire clk,
     input  wire reset,
@@ -21,37 +14,44 @@ module top_module (
     output reg  start_shifting
 );
 
-  reg [2:0] state = `STATE_X0, next_state = `STATE_X0;
+  parameter STATE_X0 = 3'b000;
+  parameter STATE_X1 = 3'b001;
+  parameter STATE_X2 = 3'b010;
+  parameter STATE_X3 = 3'b011;
+  parameter STATE_F0 = 3'b100;
+  parameter STATE_ILLEGAL = 3'b111;
+
+  reg [2:0] state = STATE_X0, next_state = STATE_X0;
 
   //Determine next state.
   always @(*) begin
     casez ({
       data, state
     })
-      {1'b0, `STATE_X0} : next_state = `STATE_X0;
-      {1'b1, `STATE_X0} : next_state = `STATE_X1;
-      {1'b0, `STATE_X1} : next_state = `STATE_X0;
-      {1'b1, `STATE_X1} : next_state = `STATE_X2;
-      {1'b0, `STATE_X2} : next_state = `STATE_X3;
-      {1'b1, `STATE_X2} : next_state = `STATE_X2;
-      {1'b0, `STATE_X3} : next_state = `STATE_X0;
-      {1'b1, `STATE_X3} : next_state = `STATE_F0;
-      {1'bz, `STATE_F0} : next_state = `STATE_F0;
-      default: next_state = `STATE_ILLEGAL;
+      {1'b0, STATE_X0} : next_state = STATE_X0;
+      {1'b1, STATE_X0} : next_state = STATE_X1;
+      {1'b0, STATE_X1} : next_state = STATE_X0;
+      {1'b1, STATE_X1} : next_state = STATE_X2;
+      {1'b0, STATE_X2} : next_state = STATE_X3;
+      {1'b1, STATE_X2} : next_state = STATE_X2;
+      {1'b0, STATE_X3} : next_state = STATE_X0;
+      {1'b1, STATE_X3} : next_state = STATE_F0;
+      {1'bz, STATE_F0} : next_state = STATE_F0;
+      default: next_state = STATE_ILLEGAL;
     endcase
   end
 
   //State transition.
   always @(posedge clk) begin
-    if (reset) state <= `STATE_X0;
+    if (reset) state <= STATE_X0;
     else if (clk) state <= next_state;
   end
 
   //Determine output.
   always @(*) begin
     case (state)
-      `STATE_X0, `STATE_X1, `STATE_X2, `STATE_X3: start_shifting = 1'b0;
-      `STATE_F0: start_shifting = 1'b1;
+      STATE_X0, STATE_X1, STATE_X2, STATE_X3: start_shifting = 1'b0;
+      STATE_F0: start_shifting = 1'b1;
       default: start_shifting = 1'bx;
     endcase
   end
--- HDLBits/Circuits/Building Larger Circuits/review2015_fsmshift/top_module.v
+++ HDLBits/Circuits/Building Larger Circuits/review2015_fsmshift/top_module.v
@@ -7,44 +7,44 @@ SPDX-License-Identifier: Apache-2.0
 
 `default_nettype none
 
-`define STATE_C1 3'b001
-`define STATE_C2 3'b010
-`define STATE_C3 3'b011
-`define STATE_C4 3'b100
-`define STATE_OFF 3'b101
-`define STATE_ILLEGAL 3'b111
-
 module top_module (
     input  wire clk,
     input  wire reset,
     output reg  shift_ena
 );
 
-  reg [2:0] state = `STATE_C1, next_state = `STATE_C1;
+  parameter STATE_C1 = 3'b001;
+  parameter STATE_C2 = 3'b010;
+  parameter STATE_C3 = 3'b011;
+  parameter STATE_C4 = 3'b100;
+  parameter STATE_OFF = 3'b101;
+  parameter STATE_ILLEGAL = 3'b111;
+
+  reg [2:0] state = STATE_C1, next_state = STATE_C1;
 
   //Determine next state.
   always @(*) begin
     case (state)
-      `STATE_C1: next_state = `STATE_C2;
-      `STATE_C2: next_state = `STATE_C3;
-      `STATE_C3: next_state = `STATE_C4;
-      `STATE_C4: next_state = `STATE_OFF;
-      `STATE_OFF: next_state = `STATE_OFF;
-      default: next_state = `STATE_ILLEGAL;
+      STATE_C1: next_state = STATE_C2;
+      STATE_C2: next_state = STATE_C3;
+      STATE_C3: next_state = STATE_C4;
+      STATE_C4: next_state = STATE_OFF;
+      STATE_OFF: next_state = STATE_OFF;
+      default: next_state = STATE_ILLEGAL;
     endcase
   end
 
   //State transition.
   always @(posedge clk) begin
-    if (reset) state <= `STATE_C1;
+    if (reset) state <= STATE_C1;
     else if (clk) state <= next_state;
   end
 
   //Determine output.
   always @(*) begin
     case (state)
-      `STATE_OFF: shift_ena = 1'b0;
-      `STATE_C1, `STATE_C2, `STATE_C3, `STATE_C4: shift_ena = 1'b1;
+      STATE_OFF: shift_ena = 1'b0;
+      STATE_C1, STATE_C2, STATE_C3, STATE_C4: shift_ena = 1'b1;
       default: shift_ena = 1'bx;
     endcase
   end
